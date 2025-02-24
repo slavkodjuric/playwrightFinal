0da92c2c39e873db431113e96946598b0bb116ab
slavkodjuric <slavkodjuric62@gmail.com>
2025-02-24 21:56:30 +0100

    headingsJS created

---
+++ fixtures/index.js
@@ -0,0 +1,4 @@
+export { URLS } from "./urls.js";
+export { HEADINGS } from "./headings";
+export * as utils from "./utils";
+export { generateUserCredentials, VALID_LOGIN_PAYLOAD } from "./userData";
