c08f934296a21d61d04cc7561b372afbba7c99a2
slavkodjuric <slavkodjuric62@gmail.com>
2025-02-24 21:56:49 +0100

    urlsJS created

---
+++ fixtures/urls.js
@@ -0,0 +1,5 @@
+export const URLS = {
+  LOGIN: "https://automaticityacademy.ngrok.app/login",
+  REGISTER: "https://automaticityacademy.ngrok.app/register",
+  DASHBOARD: "https://automaticityacademy.ngrok.app/dashboard",
+};
