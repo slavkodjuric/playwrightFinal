f55df2c1dc5fb5398bd0b9ca331a603faec3886c
slavkodjuric <slavkodjuric62@gmail.com>
2025-02-24 21:57:05 +0100

    utilsJS created

---
+++ fixtures/utils.js
@@ -0,0 +1,22 @@
+const generateRandomString = (length) => {
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let result = "";
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    result += characters[randomIndex];
+  }
+  return result;
+};
+
+const generateRandomNumber = (length) => {
+  const characters = "0123456789";
+  let result = "";
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    result += characters[randomIndex];
+  }
+  return result;
+};
+
+export { generateRandomString, generateRandomNumber };
