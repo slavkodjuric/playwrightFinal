2996f88437564392653f033f303bbf5cb9c5e9f1
Slavko Đurić <58055315+slavkodjuric@users.noreply.github.com>
2025-06-19 09:22:37 +0200

    Update register.spec.js

--- tests/UI/register.spec.js
+++ tests/UI/register.spec.js
@@ -6,7 +6,7 @@ import { RegisterPage } from "../../POM/modules/UI/registerUI";
 test.describe("register user using UI", () => {
   let registerPage;
   const { username, email, password } = generateUserCredentials(5);
-  test.beforeEach("visit the login page", async ({ page }) => {
+  test.beforeEach("visit the register page", async ({ page }) => {
     registerPage = new RegisterPage(page);
     await page.goto(URLS["REGISTER"]);
   });
